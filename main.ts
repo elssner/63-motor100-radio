2ddf79b8c4c224262c66492101f68f04864b995c
GTA Calliope <73078489+elssner@users.noreply.github.com>
2023-12-16 23:10:04 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,32 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    funkzeit_ms = input.runningTime()
-    motors.dualMotorPower(Motor.M0, receivedNumber)
+    if (ready) {
+        funkzeit_ms = input.runningTime()
+        motors.dualMotorPower(Motor.M0, receivedNumber)
+        if (receivedNumber < -4) {
+            basic.setLedColors(0xffffff, 0x0000ff, 0x000000)
+        } else if (receivedNumber > 4) {
+            basic.setLedColors(0x000000, 0x0000ff, 0xff0000)
+        } else {
+            basic.setLedColors(0x000000, 0x0000ff, 0x000000)
+        }
+    }
 })
 let funkzeit_ms = 0
+let ready = false
 radio.setTransmitPower(7)
 radio.setFrequencyBand(1)
 radio.setGroup(220)
-basic.showString("J")
+basic.showLeds(`
+    . . # . .
+    . # . # .
+    . # . # .
+    # # # # #
+    . # . # .
+    `)
+ready = true
 loops.everyInterval(2000, function () {
     if (input.runningTime() - funkzeit_ms > 2500) {
         motors.dualMotorPower(Motor.M0, 0)
-        basic.setLedColors(0x000000, 0xff0000, 0x000000, 20)
-    } else {
-        basic.setLedColors(0x000000, 0x0000ff, 0x000000, 20)
+        basic.setLedColors(0x00ffff, 0x000000, 0x00ffff, 5)
     }
 })
